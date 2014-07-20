92925c574d968e0cc8f92528f059ed6f73d357ba
gambleDev <jte12345@gmail.com>
2014-07-19 23:15:33 -0500

    Update main.php

--- src/application/controllers/Login/main.php
+++ src/application/controllers/Login/main.php
@@ -2,8 +2,7 @@
 
 	class main extends controller{
 		public function check($parameters){
-			$this->tryThis('helloBaby');
-			echo"You are logging in as ".$parameters['username']." with password ".$parameters['password'].$this->helloBaby;
+			echo"You are logging in as ".$parameters['username']." with password ".$parameters['password'];
 		}
 	}
 ?>
